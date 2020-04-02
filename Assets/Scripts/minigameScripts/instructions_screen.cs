2e539a663677388fa5e6f3984a9a1feaff34b76d
linksta7 <linksta7@gmail.com>
2020-04-02 16:39:08 -0400

    Eta object changed to circle, added instructions screen (minigame1)

---
+++ Assets/Scripts/minigameScripts/instructions_screen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instructions_screen : MonoBehaviour {
+
+	public GameObject instructionUI;
+
+	public void continueButton() {
+		instructionUI.SetActive (false);
+	}
+}
