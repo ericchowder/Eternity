49456398bea6673ac893d1101eea1e270320fcf6
linksta7 <linksta7@gmail.com>
2020-04-03 21:39:43 -0400

    minigame 2 done, increased maze1 collision sizes

---
+++ Assets/Scripts/minigameScripts/rotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.RotateAround (new Vector3 (3.5f, -2.5f, 0f), new Vector3 (0f, 0f, 1f), 90f * Time.deltaTime);
+	}
+}
