184d121cbcdd6504810d594620110f10696a42cf
janiceLaw <janice.law782@gmail.com>
2020-04-06 14:00:11 -0400

    Skip button fixed

--- Assets/Scripts/minigameScripts/SkipGame.cs
+++ Assets/Scripts/minigameScripts/SkipGame.cs
@@ -8,12 +8,42 @@ public class SkipGame : MonoBehaviour {
     public string minigame;
 
 
-    public void Skip()
+    public void Skip1()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(sceneToSkipTo);
         SceneManager.UnloadSceneAsync(minigame);
 
         Destroy(GameObject.Find("MinigameAudio"));
+        GameObject.FindGameObjectWithTag("Eta").GetComponent<BoxCollider2D>().enabled = true;
+
+        MemoryLense.lense.unlockedStatus1 = 1;
+        MemoryLense.lense.totalUnlocked = 1;
+    }
+
+    public void Skip2()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneToSkipTo);
+        SceneManager.UnloadSceneAsync(minigame);
+
+        Destroy(GameObject.Find("MinigameAudio"));
+        GameObject.FindGameObjectWithTag("Eta").GetComponent<BoxCollider2D>().enabled = true;
+
+        MemoryLense.lense.unlockedStatus2 = 1;
+        MemoryLense.lense.totalUnlocked = 2;
+    }
+
+    public void Skip3()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneToSkipTo);
+        SceneManager.UnloadSceneAsync(minigame);
+
+        Destroy(GameObject.Find("MinigameAudio"));
+        GameObject.FindGameObjectWithTag("Eta").GetComponent<BoxCollider2D>().enabled = true;
+
+        MemoryLense.lense.unlockedStatus3 = 1;
+        MemoryLense.lense.totalUnlocked = 3;
     }
 }
